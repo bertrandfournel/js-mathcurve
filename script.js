97f1b3f7b4aae4b11ce429bf63c0d3fc13ee9881
Bertrand Fournel <fournel.bertrand@gmail.com>
2020-03-27 20:40:03 +0100

    rajout du formulaire + boucle de dessin

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-window.onload = () => {
 
+
+window.onload = () => {
+    
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var height = canvas.getAttribute("height");
     var width = canvas.getAttribute("width");
     var steps = 20;
+    
+    
+    var btn = document.getElementById("btn");
+    
+
+    
     
 
     // Création des axes X et Y
@@ -27,25 +35,38 @@ window.onload = () => {
     //Création de la grille
     //Axe X
     for(let i = 0; i < (width); i += ((width) / (steps))){
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.moveTo(i , 0);
         ctx.lineTo(i, height);
         ctx.lineWidth = 1;
         ctx.stroke();
-        ctx.closePath()
+        ctx.closePath();
     }
     //Axe Y
     for(let i = 0; i < (height); i += ((height) / (steps))){
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.moveTo(0 , i);
         ctx.lineTo(width, i);
         ctx.lineWidth = 1;
         ctx.stroke();
-        ctx.closePath()
+        ctx.closePath();
     }
+    
+    draw('2x');
 
-    function draw(){
-        
+
+    
+    
+    function draw(formula){
+        for (let i = 399; i < width; i--) {
+            var obj = new Formula(formula);
+            var result = obj.evaluate({x: i});
+            ctx.beginPath()
+            ctx.fillStyle = 'red';
+            ctx.fillRect(i,result,2,2);
+            ctx.closePath()
+            console.log(i)
+        }
     }
 
 
